18ba50f31a3aaf0f7e4dddee7bdf94a4d0ed5cca
YQU2020 <yipeng.qu.20@ucl.ac.uk>
2023-11-30 13:10:24 -0800

    Method modification
    Change the method of dropping the statistic agents, still wip

--- vmas/examples/run_twin_tracking.py
+++ vmas/examples/run_twin_tracking.py
@@ -1,57 +1,53 @@
 import time
+from typing import Type
+
 import torch
 from vmas import make_env
 from vmas.simulator.heuristic_policy import BaseHeuristicPolicy, RandomPolicy
 from vmas.simulator.utils import save_video
-from typing import Type
-from vmas.scenarios.twin_tracking import Scenario
+from vmas.scenarios.twin_tracking import Scenario, SimplePolicy
 
-# Define the policy
-class SimplePolicy(BaseHeuristicPolicy):
-    def __init__(self, continuous_actions=True):
-        super().__init__(continuous_actions)
-        self.target_pos = None
-
-    def compute_action(self, observation: torch.Tensor, u_range: float) -> torch.Tensor:
-        if self.target_pos is None:
-            self.target_pos = torch.rand(2) * 2 - 1
-
-        pos_agent = observation[:, :2]
-        action = torch.clamp(self.target_pos - pos_agent, min=-u_range, max=u_range)
-        return action
-
-# Run the heuristic with the custom scenario
 def run_twin_tracking(
-    scenario_class,
-    heuristic: Type[BaseHeuristicPolicy] = SimplePolicy,
+    scenario_name: str,
+    heuristic: Type[BaseHeuristicPolicy] = RandomPolicy,
     n_steps: int = 200,
-    n_envs: int = 2,
-    render: bool = True,
+    n_envs: int = 32,
+    env_kwargs: dict = {},
+    render: bool = False,
     save_render: bool = False,
     device: str = "cpu",
 ):
+
     assert not (save_render and not render), "To save the video you have to render it"
 
+    # Scenario specific variables
     policy = heuristic()
-    
-    twin_scenario = scenario_class()
-    # Initialize the environment with the custom scenario
+
     env = make_env(
-        scenario=twin_scenario,
+        scenario=scenario_name,
         num_envs=n_envs,
         device=device,
         continuous_actions=True,
         wrapper=None,
+        # Environment specific variables
+        **env_kwargs,
     )
-    
-    frame_list = []
+
+    frame_list = []  # For creating a gif
     init_time = time.time()
     step = 0
     obs = env.reset()
     total_reward = 0
 
-    # Start laying the trail
-    twin_scenario.start_trail()
+    # Initialize the trail
+    if scenario_name == "twin_tracking":
+        twin_scenario = Scenario()
+        twin_scenario._world = env.world  # Set the world attribute
+        twin_scenario.make_world(n_envs, device)  # Explicitly call make_world
+        twin_scenario.start_trail()
+    
+    last_position = None
+    trail_interval = 10  # Number of steps after which to add a static agent
 
     for s in range(n_steps):
         step += 1
@@ -59,7 +55,17 @@ def run_twin_tracking(
         for i in range(len(obs)):
             actions[i] = policy.compute_action(obs[i], u_range=env.agents[i].u_range)
         obs, rews, dones, info = env.step(actions)
-        twin_scenario.update_trail(env.agents[0])  # Update the trail based on the agent's movement
+
+        # Update the trail for twin_tracking scenario
+        if scenario_name == "twin_tracking":
+            twin_scenario.update_trail(env.agents[0])
+            
+        if s % trail_interval == 0:
+            current_position = env.world.agents[0].state.pos.clone()
+            if last_position is not None:
+                twin_scenario.add_static_agent(last_position)
+            last_position = current_position
+
         rewards = torch.stack(rews, dim=1)
         global_reward = rewards.mean(dim=1)
         mean_global_reward = global_reward.mean(dim=0)
@@ -73,25 +79,26 @@ def run_twin_tracking(
                 )
             )
 
-    # Stop laying the trail
-    twin_scenario.stop_trail()
+    # Stop the trail
+    if scenario_name == "twin_tracking":
+        twin_scenario.stop_trail()
 
     total_time = time.time() - init_time
     if render and save_render:
-        save_video("custom_scenario", frame_list, 1 / env.scenario.world.dt)
+        save_video(scenario_name, frame_list, 1 / env.scenario.world.dt)
 
     print(
         f"It took: {total_time}s for {n_steps} steps of {n_envs} parallel environments on device {device}\n"
         f"The average total reward was {total_reward}"
     )
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     run_twin_tracking(
-        scenario_class= Scenario,
+        scenario_name="twin_tracking",
         heuristic=SimplePolicy,
-        n_envs=2,
-        n_steps=600,
+        n_envs=300,
+        n_steps=200,
         render=True,
         save_render=False,
     )
--- vmas/scenarios/twin_tracking.py
+++ vmas/scenarios/twin_tracking.py
@@ -4,74 +4,65 @@ from vmas.simulator.core import Agent, World, Sphere
 from vmas.simulator.scenario import BaseScenario
 from vmas.simulator.utils import TorchUtils
 
-# Extends the Agent class to support trail laying functionality
-class TrailLayingAgent(Agent):
-    def __init__(self, name, u_multiplier, shape):
-        super().__init__(name, u_multiplier, shape)
-        # Initialize trail-related attributes
-        self.trail_active = False
-        self.trail_points = []
-        self.last_point = None
-        self.trail_distance = 0.1  # Default distance between trail points
-
-    # Starts laying the trail
-    def start_trail(self):
-        self.trail_active = True
-        self.last_point = self.state.pos.clone()
-        self.trail_points.append(self.last_point)
-
-    # Stops laying the trail
-    def stop_trail(self):
-        self.trail_active = False
-        self._add_trail_point(self.state.pos)
-
-    # Updates the trail as the agent moves
-    def update_trail(self):
-        if self.trail_active and (self.last_point is None or torch.norm(self.state.pos - self.last_point) >= self.trail_distance):
-            self._add_trail_point(self.state.pos)
-
-    # Adds a new point to the trail
-    def _add_trail_point(self, new_point):
-        if self.last_point is not None:
-            line = (self.last_point.clone(), new_point.clone())
-            self.trail_points.append(line)
-        self.last_point = new_point.clone()
-
-# Scenario class for the simulation environment
 class Scenario(BaseScenario):
     def make_world(self, batch_dim: int, device: torch.device, **kwargs):
         world = World(batch_dim, device)
-        # Create a TrailLayingAgent
-        agent = TrailLayingAgent(name="agent_0", u_multiplier=1.0, shape=Sphere(0.03))
+        agent = Agent(name="agent_0", u_multiplier=1.0, shape=Sphere(0.03))
         world.add_agent(agent)
 
-        # Set a goal position for the agent
         self.goal_pos = torch.tensor([0.5, 0.5], device=device)
+        self.trail_active = False
+        self.trail_points = []
+        self.last_point = None
+        self.trail_distance = 0.1  # Default distance, can be changed
+
         return world
 
-    # Resets the world and agents for a new simulation
     def reset_world_at(self, env_index: int = None):
         for agent in self.world.agents:
-            # Reset agent position and trail
             agent.set_pos(
                 torch.zeros((1, self.world.dim_p), device=self.world.device).uniform_(-1.0, 1.0),
                 batch_index=env_index
             )
-            agent.trail_points.clear()
-            agent.last_point = None
-            agent.trail_active = False
+            self.trail_points.clear()
+            self.last_point = None
+            self.trail_active = False
 
-    # Calculates the reward for the agent based on its distance to the goal
     def reward(self, agent: Agent):
         distance_to_goal = torch.norm(agent.state.pos - self.goal_pos)
         return -distance_to_goal.unsqueeze(0)
 
-    # Generates the observation for the agent
     def observation(self, agent: Agent):
         expanded_goal_pos = self.goal_pos.unsqueeze(0).expand(agent.state.pos.size(0), -1)
         return torch.cat([agent.state.pos, agent.state.vel, expanded_goal_pos], dim=-1)
 
-# Simple policy for agent's actions
+    def update_trail(self, agent: Agent):
+        if self.trail_active and (self.last_point is None or torch.norm(agent.state.pos - self.last_point) >= self.trail_distance):
+            if self.last_point is not None:
+                line = (self.last_point.clone(), agent.state.pos.clone())
+                self.trail_points.append(line)
+            self.last_point = agent.state.pos.clone()
+
+    def start_trail(self):
+        self.trail_active = True
+        self.last_point = self.world.agents[0].state.pos.clone()
+        self.trail_points.append(self.last_point)
+
+    def stop_trail(self):
+        self.trail_active = False
+        self.update_trail(self.world.agents[0])
+    
+    #**********************************************************************************************************************
+    # Add a static agent to the world
+    def add_static_agent(self, position):
+            static_agent = Agent(name=f"static_{len(self.world.agents)}", u_multiplier=0.0, shape=Sphere(0.03))
+            self.world.add_agent(static_agent)  # Add the agent to the world first
+
+            # Extract the relevant slice from the position tensor for a single agent
+            single_agent_position = position[0]
+            static_agent.set_pos(single_agent_position, batch_index=0)
+            
+
 class SimplePolicy:
     def compute_action(self, observation: torch.Tensor, u_range: float) -> torch.Tensor:
         pos_agent = observation[:, :2]
@@ -79,30 +70,11 @@ class SimplePolicy:
         action = torch.clamp(goal_pos - pos_agent, min=-u_range, max=u_range)
         return action
 
-# Main execution block
 if __name__ == "__main__":
-    # Render the simulation interactively
+    
     render_interactively(
         __file__,
         desired_velocity=0.05,
-        n_agents=1,
+        n_agents=10,
+        
     )
-
-
-'''
-Current problem:
-Traceback (most recent call last):
-  File "twin_tracking.py", line 85, in <module>
-    render_interactively(
-  File "/home/willqu/VectorizedMultiAgentSimulator/vmas/interactive_rendering.py", line 316, in render_interactively
-    InteractiveEnv(
-  File "/home/willqu/VectorizedMultiAgentSimulator/vmas/interactive_rendering.py", line 76, in __init__
-    self.env.render()
-  File "/home/willqu/VectorizedMultiAgentSimulator/vmas/simulator/environment/gym.py", line 79, in render
-    return self._env.render(
-  File "/home/willqu/VectorizedMultiAgentSimulator/vmas/simulator/environment/environment.py", line 567, in render
-    [agent.shape.circumscribed_radius() for agent in self.world.agents]
-  File "/home/willqu/VectorizedMultiAgentSimulator/vmas/simulator/environment/environment.py", line 567, in <listcomp>
-    [agent.shape.circumscribed_radius() for agent in self.world.agents]
-AttributeError: 'float' object has no attribute 'circumscribed_radius'
-'''
