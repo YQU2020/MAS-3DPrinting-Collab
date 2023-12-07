02b2bff079844bacf488bf29430542220a46bad0
YQU2020 <yipeng.qu.20@ucl.ac.uk>
2023-12-07 13:12:19 -0800

    Troubleshooting 2

--- vmas/scenarios/single_agent_scenario.py
+++ vmas/scenarios/single_agent_scenario.py
@@ -2,7 +2,7 @@ import torch
 from vmas import render_interactively
 from vmas.simulator.core import Agent, World, Sphere, Landmark
 from vmas.simulator.scenario import BaseScenario
-from vmas.simulator.utils import Color
+from vmas.simulator.utils import TorchUtils, Color
 
 class Scenario(BaseScenario):
     def make_world(self, batch_dim: int, device: torch.device, **kwargs):
@@ -10,22 +10,19 @@ class Scenario(BaseScenario):
         agent = Agent(name="agent_0", u_multiplier=1.0, shape=Sphere(0.03))
         world.add_agent(agent)
 
-        # Set a random goal position
-        self.goal_pos = torch.rand(2) * 2 - 1  # Random position between -1 and 1 for both x and y
-        print(f"Goal position: {self.goal_pos}")
-
-        # Add a landmark to visually represent the goal position
-        goal_landmark = Landmark(
-            name="goal_landmark",
+        # Add a landmark to visually represent the target position
+        self.landmark = Landmark(
+            name="target_landmark",
             collide=False, 
             shape=Sphere(radius=0.03),
             color=Color.GREEN,
         )
-        world.add_landmark(goal_landmark)
-        goal_landmark.set_pos(self.goal_pos.unsqueeze(0), batch_index=0)
-
+        world.add_landmark(self.landmark)
+        
         # Issue: The landmark appears at the center of the screen despite being set to a random position.
         # Potential causes could be incorrect initialization, rendering logic, or coordinate system issues.
+        # Removed the landmark position initialization from the reset_world_at function to see if it would fix the issue.
+
 
         return world
 
@@ -42,32 +39,30 @@ class Scenario(BaseScenario):
             )
 
         # Randomly initialize the landmark's position
-        for landmark in self.world.landmarks:
-            landmark.set_pos(
-                torch.zeros(
-                    (1, self.world.dim_p) if env_index is not None else (self.world.batch_dim, self.world.dim_p),
-                    device=self.world.device,
-                    dtype=torch.float32,
-                ).uniform_(-1.0, 1.0),
-                batch_index=env_index
-            )
+        self.landmark.set_pos(
+            torch.zeros(
+                (1, self.world.dim_p) if env_index is not None else (self.world.batch_dim, self.world.dim_p),
+                device=self.world.device,
+                dtype=torch.float32,
+            ).uniform_(-1.0, 1.0),
+            batch_index=env_index
+        )
 
-        # Issue: Ensure that the landmark's position is being updated correctly and not overridden elsewhere.
-
-    # Reward based on the distance to the goal, note that the reward is negative, no problem with that
     def reward(self, agent: Agent):
-        distance_to_goal = torch.norm(agent.state.pos - self.goal_pos)
-        return -distance_to_goal.unsqueeze(0)
+        # Reward based on the distance to the landmark
+        distance_to_landmark = torch.norm(agent.state.pos - self.landmark.state.pos)
+        return -distance_to_landmark.unsqueeze(0)  # Negative reward for distance
 
     def observation(self, agent: Agent):
-        expanded_goal_pos = self.goal_pos.unsqueeze(0).expand(agent.state.pos.size(0), -1)
-        return torch.cat([agent.state.pos, agent.state.vel, expanded_goal_pos], dim=-1)
+        # Observation includes agent's position, velocity, and relative position to the landmark
+        return torch.cat([agent.state.pos, agent.state.vel, self.landmark.state.pos - agent.state.pos], dim=-1)
 
 class SimplePolicy:
     def compute_action(self, observation: torch.Tensor, u_range: float) -> torch.Tensor:
+        # Extract agent position and landmark position from the observation
         pos_agent = observation[:, :2]
-        goal_pos = observation[:, -2:]
-        action = torch.clamp(goal_pos - pos_agent, min=-u_range, max=u_range)
+        landmark_pos = observation[:, -2:]
+        action = torch.clamp(landmark_pos - pos_agent, min=-u_range, max=u_range)
         return action
 
 if __name__ == "__main__":
@@ -76,4 +71,3 @@ if __name__ == "__main__":
         desired_velocity=0.05,
         n_agents=1,
     )
-1
